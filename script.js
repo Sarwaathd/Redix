fd9a6216bc1ba54fc84fef4eca592f30ee47397e
Sarwaathd <sakibahmmed0028@gmail.com>
2025-11-13 11:43:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const welcomeBtn = document.getElementById('welcomeBtn');
 const aboutPage = document.getElementById('aboutPage');
 
-welcomeBtn.addEventListener('click', function() {
-    aboutPage.style.transform = 'translateY(0)';
+welcomeBtn.addEventListener('click', () => {
+  aboutPage.style.transform = 'translateY(0)';
 });
